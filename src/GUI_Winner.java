4064ffb5e6af74e6be29ce7ffc660b1126ad75cc
subl <pwan@seas.upenn.edu>
2014-04-12 15:59:49 -0400

    finished question 2

--- src/GUI_Winner.java
+++ src/GUI_Winner.java
@@ -4,67 +4,8 @@ import java.awt.event.*;
 import java.io.*;
 import javax.sound.sampled.*;
 
-public class GUI_Winner implements ActionListener
-{
-    private JFrame parentFrame;
-    private JFrame congratulationsFrame;
-    private JLabel answerLabel;
-    private JLabel secretWordLabel;
-    private JLabel gameResultLabel;
-    private JButton returnBtn;
-    private ImageIcon background;
-    private JPanel imagePanel;
-    
-    public GUI_Winner(String Letters,JFrame frame)
-    {
-        parentFrame = frame;
-        congratulationsFrame = new JFrame("You are the winner!!!");
-        bg(congratulationsFrame);
-        answerLabel = new JLabel("The answer is ");
-        
-        secretWordLabel = new JLabel(Letters);
-        secretWordLabel.setFont(new Font("Default",Font.PLAIN,23));
-        secretWordLabel.setForeground(Color.red);
-        gameResultLabel = new JLabel("You are winner!");
-        returnBtn = new JButton("Return to the main menu");
-
-        returnBtn.addActionListener(this); 
-        
-        congratulationsFrame.add(answerLabel);
-        congratulationsFrame.add(secretWordLabel);
-        congratulationsFrame.add(gameResultLabel);
-        congratulationsFrame.add(returnBtn);
-
-        congratulationsFrame.setVisible(true);
-
-    }
-
-    public void bg(JFrame frame)
-    {
-        background = new ImageIcon("Congrats.gif");
-        JLabel label = new JLabel(background);
-     
-        label.setBounds(0, 0, background.getIconWidth(),
-            background.getIconHeight());
-   
-        imagePanel = (JPanel) frame.getContentPane();
-        imagePanel.setOpaque(false);
-
-        imagePanel.setLayout(new FlowLayout());
-
-        frame.getLayeredPane().setLayout(null);
-
-        frame.getLayeredPane().add(label, new Integer(Integer.MIN_VALUE));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(background.getIconWidth(), background.getIconHeight());
-        frame.setResizable(false);
-
-    }
-
-    public void actionPerformed(ActionEvent e)
-    {
-        congratulationsFrame.dispose();
-        parentFrame.dispose();
-    	new Start().createAndShowGUI();
+public class GUI_Winner extends ResultFrame{   
+    public GUI_Winner(String letters,JFrame frame){
+        super(letters, frame, "You are the winner!!!", "Congrats.gif");
     }
 }
---
+++ src/ResultFrame.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public abstract class ResultFrame implements ActionListener {
+	protected JFrame parentFrame;
+	protected JFrame resultFrame;
+    protected JLabel answerLabel;
+    protected JLabel gameResultLabel;
+    protected JButton returnBtn;
+    
+    public ResultFrame(String letters, JFrame frame, String prompt, String imagePath) {
+    	parentFrame = frame;
+    	resultFrame = new JFrame(prompt);
+    	resultFrame.setLayout(new FlowLayout());
+    	resultFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	answerLabel = new JLabel("The answer is " + letters);
+    	answerLabel.setFont(new Font("Default",Font.PLAIN,23));
+        answerLabel.setForeground(Color.red);
+        gameResultLabel = new JLabel(prompt);
+        returnBtn = new JButton("Return to the main menu");
+        
+        returnBtn.addActionListener(this);
+        
+        ImageIcon resultIcon = new ImageIcon(imagePath);
+        JLabel resultLabel = new JLabel(resultIcon);
+    	resultFrame.setSize(new Dimension(Math.max(resultIcon.getIconWidth(), answerLabel.getWidth()),470));
+        System.out.println(resultIcon.getIconWidth());
+        System.out.println(Math.max(resultIcon.getIconWidth(), answerLabel.getWidth()));
+    	
+        resultFrame.add(answerLabel);
+        resultFrame.add(gameResultLabel);
+        resultFrame.add(returnBtn);
+        resultFrame.add(resultLabel);
+        
+        resultFrame.setResizable(false);
+    	resultFrame.setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        resultFrame.dispose();
+        parentFrame.dispose();
+    	new Start().createAndShowGUI();
+    }	
+}
